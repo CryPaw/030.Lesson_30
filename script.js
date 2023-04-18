23530bc3bf9909c4fafaa9cbefed9d2da4dd9511
Nick <119486388+CryPaw@users.noreply.github.com>
2023-04-18 14:11:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,70 +1,49 @@
-function Person (name, age) {
-    this.name = name;
-    this.age = age;
-    this.personInfo = function () {
-        console.group (`${this.name} info:`);
-        console.log ("Your name is: " + this.name);
-        console.log ("Your age is: " + this.age);
-        console.groupEnd ();
-    };
-}; 
-
-function Car (brand,model,year,licenseplate) {
-    this.brand = brand;
-    this.model = model;
-    this.year = year;
-    this.licenseplate = licenseplate;
-    this.getCarInfo = function () {
-        console.group (`${this.brand + ' ' + this.model} info:`);
-        console.log ("Model of your car: " + this.brand + " " + this.model);
-        console.log ("licenseplate of your car: " + this.licenseplate);
-        console.log ("Year of issue: " + this.year);
-        console.groupEnd();
-    };
-    this.ownerCar = function (person) {
-        console.log(person.name + " is the owner of the "+ this.brand + " " + this.model + " " + this.year);
-    };
-};
-
-
-const owner = new Person ("Nick", 17);
-const owner2 = new Person ("Dan", 19);
-const owner3 = new Person ("Vasya", 16);
-const owner4 = new Person ("Sergey", 36);
-
-const myCar = new Car( "BMW", "i8", 2022, "AY 2323 OG" );
-const myCar2 = new Car( "Audi", "r8", 2007, "DP 3033 PD" );
-const myCar3 = new Car( "Chevrolet", "Viper", 2007, "CH 1133 VI" );
-const myCar4 = new Car( "Ferrari", "Daytona", 2022, "FD 7777 GU" );
-
-owner.personInfo();
-if (owner.age > 18) {
-    myCar.getCarInfo();
-    myCar.ownerCar(owner);
-    } else {
-        console.log("Вам ще не положено автівку!")
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
     }
-
-    owner2.personInfo();
-    if (owner2.age > 18) {
-        myCar2.getCarInfo();
-        myCar2.ownerCar(owner2);
-        } else {
-            console.log("Вам ще не положено автівку!")
-        }
-
-        owner3.personInfo();
-    if (owner3.age > 18) {
-        myCar3.getCarInfo();
-        myCar3.ownerCar(owner3);
-        } else {
-            console.log("Вам ще не положено автівку!")
-        }
-
-        owner4.personInfo();
-        if (owner4.age > 18) {
-            myCar4.getCarInfo();
-            myCar4.ownerCar(owner4);
-            } else {
-                console.log("Вам ще не положено автівку!")
-            }
+  
+    get info() {
+      console.log(`PERSON: ${this.name} ${this.age}`)
+    }
+  }
+  
+  class Car {
+    constructor(brand, model, year, licenseplate) {
+      this.brand = brand;
+      this.model = model;
+      this.year = year;
+      this.licenseplate = licenseplate;
+    }
+  
+    owner(owner) {
+      if (owner.age < 18) {
+        console.log('ACCESS DENIED: You are too young')
+      } else {
+        this.ownerName = owner;
+      }
+    }
+  
+    get carInfo() {
+      console.log(`CAR: ${this.brand} ${this.model} ${this.year} ${this.licenseplate}`);
+      this.ownerName ? this.ownerName.info : console.log(`У ${this.brand}-${this.model} власника немає`);
+    }
+  }
+  
+  const nick = new Person('Nick', 23);
+  const dan = new Person('Dan', 12);
+  const sergey = new Person('Sergey', 36);
+  nick.info;
+  dan.info;
+  const bmw = new Car('Bmw', 'i8', 2022, 'Y2323OG');
+  const audi = new Car('Audi', 'r8', 2007, 'P3033PD');
+  const chevrolet = new Car('Chevrolet', 'Viper', 2019, 'H1133VI');
+  
+  bmw.owner(nick);
+  audi.owner(dan);
+  chevrolet.owner(sergey);
+  
+  bmw.carInfo;
+  audi.carInfo;
+  chevrolet.carInfo;
